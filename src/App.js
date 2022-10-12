76bed51459f907d5152aedc114ff8badeae48f1e
Milton Mariani <miltonmariani7@gmail.com>
2022-10-12 15:12:25 +0200

    added button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
-<Nav />
+{/* <Nav />
 <Home />
-<Footer />
+<Footer /> */}
--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,1 +1,19 @@
-import 'buttons.scss'
+import './Buttons.scss' ;
+import React, {useState} from "react";
+
+
+export default function Button(props) {
+
+    return(
+        <button className={props.style} onClick={} >
+            {props.text}
+        </button>
+    )
+}
+
+
+
+
+
+
+
