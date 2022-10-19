dcc4f97c6e2d4c4df204fd2aa0546608984c745f
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-10-19 18:10:44 +0200

    implemented images in win-prizes section

--- src/layouts/home-win-prizes/WinPrizes.js
+++ src/layouts/home-win-prizes/WinPrizes.js
@@ -1,22 +1,36 @@
 import React from "react";
 import './_win-prizes.scss';
+// import images
+import ball2 from '../../resources/ball2-1024.webp';
+import ball4 from '../../resources/ball4-1024.webp';
+import ball6 from '../../resources/ball6-1024.webp';
+import ball7 from '../../resources/ball7-1024.webp';
+import ball9 from '../../resources/ball9-1024.webp';
+
+import cardTopRight from '../../resources/card-top-right-1024.webp';
+import cardGreen from '../../resources/card-green-1024.webp';
+import cardRed from '../../resources/card-red-1024.webp';
+import cardBottomLeft from '../../resources/card-bottom-left-1024.webp';
+
+import crystalBall from '../../resources/svgs/crystal-ball.svg';
+import ticket from '../../resources/svgs/ticket.svg';
 
 export function WinPrizes() {
 
     return (
         <section class="win-prizes">
             <div class="floating-balls">
-                <img src="../../resources/ball2-1024.webp" alt="Lottery ball number 2" id="ball-n2" />
-                <img src="../../../resources/ball4-1024.webp" alt="Lottery ball number 4" id="ball-n4" />
-                <img src="../../../resources/ball6-1024.webp" alt="Lottery ball number 6" id="ball-n6" />
-                <img src="../../../resources/ball7-1024.webp" alt="Lottery ball number 7" id="ball-n7" />
-                <img src="../../../resources/ball9-1024.webp" alt="Lottery ball number 9" id="ball-n9" />
+                <img src={ball2} alt="Lottery ball number 2" id="ball-n2" />
+                <img src={ball4} alt="Lottery ball number 4" id="ball-n4" />
+                <img src={ball6} alt="Lottery ball number 6" id="ball-n6" />
+                <img src={ball7} alt="Lottery ball number 7" id="ball-n7" />
+                <img src={ball9} alt="Lottery ball number 9" id="ball-n9" />
             </div>
             <div class="floating-cards">
-                <img src="../../../resources/card-top-right-1024.webp" alt="Card top right (placeholder)" id="card-top-right" />
-                <img src="../../../resources/card-green-1024.webp" alt="card green" id="card-green" />
-                <img src="../../../resources/card-red-1024.webp" alt="card red" id="card-red" />
-                <img src="../../../resources/card-bottom-left-1024.webp" alt="card bottom left" id="card-bottom-left" />
+                <img src={cardTopRight} alt="Card top right (placeholder)" id="card-top-right" />
+                <img src={cardGreen} alt="card green" id="card-green" />
+                <img src={cardRed} alt="card red" id="card-red" />
+                <img src={cardBottomLeft} alt="card bottom left" id="card-bottom-left" />
             </div>
             <div class="prizes-wrapper">
                 <h2><span>Win</span> millions in prizes</h2>
@@ -28,7 +42,7 @@ export function WinPrizes() {
                         <div class="card-content">
 
                             <div class="card-img">
-                                <img src="../../../resources/svgs/crystal-ball.svg" alt="Crystal ball" />
+                                <img src={crystalBall} alt="Crystal ball" />
                             </div>
                             <h3>Prediction</h3>
                             <h2>$<span id="value-prediction">800</span> million</h2>
@@ -47,7 +61,7 @@ export function WinPrizes() {
                         <div class="card-content">
 
                             <div class="card-img">
-                                <img src="../../../resources/svgs/ticket.svg" alt="Ticket" />
+                                <img src={ticket} alt="Ticket" />
                             </div>
                             <h3>Lottery</h3>
                             <h2>$<span id="value-lottery">120000</span></h2>
