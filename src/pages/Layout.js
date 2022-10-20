d1b3758d69dc77934e38891fd25880cab9b54f81
giobruno <giobruno@me.com>
2022-10-20 16:37:50 +0200

    start navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,24 +1,11 @@
 import React from "react";
-import "../components/navbar/Navbar.scss"
 
-import {Outlet, Link} from 'react-router-dom'
+import Navbar from "../components/navbar/Navbar";
 
 const Layout = () => {
     return(
-        <>
-            {/* <h3>Logo</h3> */}
-            <li><Link to="/">Pancake Swap</Link></li>
-            <nav className="menu-ul menu-nav">
-                
-                <ul>
-                {/* <li><Link to="/">Home</Link></li> */}
-                <li><Link to="/trade">Trade</Link></li>
-                <li><Link to="/earn">Earn</Link></li>  
-                <li><Link to="/win">Win</Link>  </li>     
-                </ul>   
-            </nav> 
-            <Outlet></Outlet>
-        </>
+        <Navbar />
+
     )
 }
 
