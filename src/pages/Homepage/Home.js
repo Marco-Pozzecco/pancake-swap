5f174919ee5850ccd2ac22e13727bc03843e5999
otnd <davide.morrone01@gmail.com>
2022-10-27 17:34:32 +0200

    fix: trade anything

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import { WinPrizes } from "../../layouts/home-win-prizes/WinPrizes";
 import FloatingBunny from "../../layouts/home-floating-bunny/FloatingBunny";
 import StartInSeconds from "../../layouts/home-start-in-seconds/StartInSeconds"
+import TradeAnything from "../../layouts/home-trade-anything/TradeAnything"
 
 export default function Home() {
     return (
         <>
             <FloatingBunny/>
+            <TradeAnything/>
             <WinPrizes />
             <StartInSeconds/>
         </>
