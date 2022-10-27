da003ab7dfd8f382273ee4f332c15a8cd6d0411d
Clara Schillaci <dhdjjk>
2022-10-27 10:57:02 +0200

    bunny without class

--- src/layouts/home-floating-bunny/FloatingBunny.js
+++ src/layouts/home-floating-bunny/FloatingBunny.js
@@ -8,33 +8,33 @@ import stella3 from "./stella3.webp";
 export default function FloatingBunny() {
     return (
         <section className="fb-floating-bunny">
-            <div class="fb-wrapper">
-                <div class="fb-contents-section">
+            <div className="fb-wrapper">
+                <div className="fb-contents-section">
                     <h2>The moon is made of pancakes.</h2>
                     <h3>Trade, earn, and win crypto on the most popular decentralized platform in the galaxy.</h3>
-                    <div class="fb-buttons-section">
-                        <a href="" class="link-aquagreen">
-                            <button class="button-aquagreen buttons-home">Connect Wallet</button>
+                    <div className="fb-buttons-section">
+                        <a href="" className="link-aquagreen">
+                            <button className="button-aquagreen buttons-home">Connect Wallet</button>
                         </a>
 
-                        <a href="" class="link-onlyborder">
-                            <button class="button-onlyborder buttons-trade-swap-grafico">Trade Now</button>
+                        <a href="" className="link-onlyborder">
+                            <button className="button-onlyborder buttons-trade-swap-grafico">Trade Now</button>
                         </a>
                     </div>
                 </div>
 
-                <div class="fb-img-section">
-                    <div class="fb-thefloatingbunny">
+                <div className="fb-img-section">
+                    <div className="fb-thefloatingbunny">
                             <img src={bunny}/>
                     </div>
-                    <div class="fb-thefloatingstars">
-                        <div class="fb-star1">
+                    <div className="fb-thefloatingstars">
+                        <div className="fb-star1">
                             <img src={stella1}/>
                         </div>
-                        <div class="fb-star2">
+                        <div className="fb-star2">
                             <img src={stella2}/>
                         </div>
-                        <div class="fb-star3">
+                        <div className="fb-star3">
                             <img src={stella3}/>
                         </div>
                     </div>
@@ -42,9 +42,9 @@ export default function FloatingBunny() {
 
             </div>
 
-            <div class="fb-bottomgradient">
+            <div className="fb-bottomgradient">
                 <svg viewBox="0 0 1660 339" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"
-                    class="sc-8a800401-0 Mvnec">
+                    className="sc-8a800401-0 Mvnec">
                     <path
                         d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z"
                         fill="url(#paint0_linear_dark)"></path>
