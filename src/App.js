61bc6de4a56743d694971300cf61620517e6c164
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-10-27 11:17:38 +0200

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
-import Trade from "./pages/Trade"
-import Earn from "./pages/Earn"
-import Win from "./pages/Win"
+import Trade from "./pages/trade/Trade"
+import Earn from "./pages/earn/Earn"
+import Win from "./pages/win/Win"
 import Home from "./pages/Home";
-import NoPage from "./pages/NoPage";
+import NoPage from "./pages/notFound/NotFound404";
 import "./App.css"
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Layout from "./pages/Layout";
