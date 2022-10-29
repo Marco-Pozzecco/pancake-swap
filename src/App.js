bd90375cf95ce1114e36e33458cb198c3972d6ab
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-10-29 11:34:55 +0200

    prettier setup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React from "react";
-import {Trade} from "./pages/trade/Trade"
-import {Earn} from "./pages/earn/Earn"
+import { Trade } from "./pages/trade/Trade"
+import { Earn } from "./pages/earn/Earn"
 import Win from "./pages/win/Win"
 import Home from "./pages/homepage/Home";
 import NoPage from "./pages/notFound/NotFound404";
 import "../src/main.scss"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {Layout} from "./pages/Layout";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Layout } from "./pages/Layout";
 // import {Footer} from "../src/components/footer/Footer"
 
 
 export function App() {
-    return (  
-            <>
-             <Router>
+    return (
+        <>
+            <Router>
                 <Routes>
                     <Route path="/" element={<Layout />} >
-                    <Route index element={<Home />} />
-                    <Route path="trade" element={<Trade/>} />
-                    <Route path="earn" element={<Earn/>} />
-                    <Route path="win" element={<Win/>} />
-                    {/* <Route path="win" element={<Nft/>} /> */}
-                    <Route path="*" element={<NoPage/>} />
+                        <Route index element={<Home />} />
+                        <Route path="trade" element={<Trade />} />
+                        <Route path="earn" element={<Earn />} />
+                        <Route path="win" element={<Win />} />
+                        {/* <Route path="win" element={<Nft/>} /> */}
+                        <Route path="*" element={<NoPage />} />
                     </Route>
                 </Routes>
-                </Router>
-                
-                {/* <Home/> */}
-                {/* <Footer/> */}
-                </>
+            </Router>
+
+            {/* <Home/> */}
+            {/* <Footer/> */}
+        </>
     );
-    
+
 }
-            
-            
+
--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { WinPrizes } from "../../layouts/home-win-prizes/WinPrizes";
 import Subnav from '../../components/subnav/Subnav';
+import { FloatingBunny } from "../../layouts/home-floating-bunny/FloatingBunny";
 
 // import { Page } from "../../components/page/Page";
 // import Subnav from "../../components/subnav/Subnav";
@@ -13,7 +14,9 @@ export default function Home() {
         // </Page>
         <>
             <Subnav/>
+            <FloatingBunny />
             <WinPrizes />
+            
         </>
         
   
