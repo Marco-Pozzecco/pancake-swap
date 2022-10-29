75f3bde842f1873d92c8d552e07dbb53a2ebf6cf
giobruno <giobruno@me.com>
2022-10-29 16:18:04 +0200

    add color to body

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React from "react";
-import {Trade} from "./pages/trade/Trade"
-import {Earn} from "./pages/earn/Earn"
-import {Win} from "./pages/win/Win"
+import {Trade} from "./pages/trade/Trade";
+import {Earn} from "./pages/earn/Earn";
+import {Win} from "./pages/win/Win";
 import Home from "./pages/homepage/Home";
 import {NoPage} from "./pages/notFound/NotFound404";
-import "../src/main.scss"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import "../src/main.scss";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Layout} from "./pages/Layout";
-import { Nft } from "./pages/NFT/Nft";
-
-
+import {Nft} from "./pages/NFT/Nft";
 
 export function App() {
     return (
-        <>
+        <div>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Layout />} >
-                    <Route index element={<Home />} />
-                    <Route path="trade" element={<Trade/>} />
-                    <Route path="earn" element={<Earn/>} />
-                    <Route path="win" element={<Win/>} />
-                    <Route path="nft" element={<Nft/>} />
-                    {/* <Route path="win" element={<extra/>} /> */}
-                    <Route path="*" element={<NoPage/>} />
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="trade" element={<Trade />} />
+                        <Route path="earn" element={<Earn />} />
+                        <Route path="win" element={<Win />} />
+                        <Route path="nft" element={<Nft />} />
+                        {/* <Route path="win" element={<extra/>} /> */}
+                        <Route path="*" element={<NoPage />} />
                     </Route>
                 </Routes>
-                </Router>
-                
-                </>
-
+            </Router>
+        </div>
     );
-
 }
-
--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { WinPrizes } from "../../layouts/home-win-prizes/WinPrizes";
-import { FloatingBunny } from "../../layouts/home-floating-bunny/FloatingBunny";
-
-
+import {WinPrizes} from "../../layouts/home-win-prizes/WinPrizes";
+import {FloatingBunny} from "../../layouts/home-floating-bunny/FloatingBunny";
 
 export default function Home() {
     return (
-
-        <>
-            
+        <div>
             <FloatingBunny />
             <WinPrizes />
-
-        </>
-
-
+        </div>
     );
 }
