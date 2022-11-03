b9ce79a06511051f1929218ca6e3f842d6aeb510
gfrise <104461249+gfrise@users.noreply.github.com>
2022-11-03 18:11:28 +0100

    feat refactoring-home-earn

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import { WinPrizes } from "../../layouts/home-win-prizes/WinPrizes";
 import { FloatingBunny } from "../../layouts/home-floating-bunny/FloatingBunny";
 import { Page } from "../../components/page/Page";
+import HomeEarn from "../../layouts/home-earn-section/home-earn";
 
 export default function Home() {
     return (
         <Page>
             <FloatingBunny />
             <WinPrizes />
+            <HomeEarn />
         </Page>
         
   
