0366f4a1227ad8d6922a8ac04e7f0666219de5d9
Milton Mariani <miltonmariani7@gmail.com>
2022-11-03 19:55:56 +0100

    start to convert main banner

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -3,6 +3,7 @@ import {WinPrizes} from "../../layouts/home-win-prizes/WinPrizes";
 import Subnav from "../../components/subnav/Subnav";
 import {FloatingBunny} from "../../layouts/home-floating-bunny/FloatingBunny";
 import {CakeToken} from "../../layouts/home-cake-token/CakeToken";
+import { MainBanner } from "../../layouts/home-main-banner/MainBanner";
 
 // import { Page } from "../../components/page/Page";
 // import Subnav from "../../components/subnav/Subnav";
@@ -13,6 +14,7 @@ export default function Home() {
             <Subnav
                 elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}
             />
+            <MainBanner />
             <FloatingBunny />
             <CakeToken />
             <WinPrizes />
