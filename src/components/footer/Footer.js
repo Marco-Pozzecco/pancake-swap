6da84b12791bfc9090860838e877f55d22aabeec
giobruno <giobruno@me.com>
2022-11-03 23:59:24 +0100

    fix bug

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -24,8 +24,8 @@ export function Footer() {
   return (
     <footer className="footer-box">
       <section className="external-box">
-      <section className="main-footer">
-        <div>
+        <section className="main-footer">
+          <div>
           <h4>About</h4>
           <ul className="menu-ul ">
             <li>
@@ -52,42 +52,42 @@ export function Footer() {
               <a href="https://pancakeswap.creator-spring.com/">Online Store</a>
             </li>
           </ul>
-        </div>
-        <div>
-          <h4>Help</h4>
-          <ul className="menu-ul">
-            <li>
-              <a href="https://docs.pancakeswap.finance/contact-us/customer-support">
-                Customer Support
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.pancakeswap.finance/help/troubleshooting">
-                Troubleshooting
-              </a>
-            </li>
-            <li>
-              <a href="https://docs.pancakeswap.finance/get-started">Guides</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h4>Developers</h4>
-          <ul className="menu-ul">
-            <li><a href="https://github.com/pancakeswap">Github</a></li>
-            <li><a href="https://docs.pancakeswap.finance/">Documentation</a></li>
-            <li><a href="https://docs.pancakeswap.finance/code/bug-bounty">Bug Bounty</a></li>
-            <li><a href="https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited">Audits</a></li>
-            <li><a href="https://docs.pancakeswap.finance/hiring/become-a-chef">Careers</a></li>
-          </ul>
-        </div>
-        <div className="flex2 logoFlex">
+          </div>
+          <div>
+            <h4>Help</h4>
+            <ul className="menu-ul">
+              <li>
+                <a href="https://docs.pancakeswap.finance/contact-us/customer-support">
+                  Customer Support
+                </a>
+              </li>
+              <li>
+                <a href="https://docs.pancakeswap.finance/help/troubleshooting">
+                  Troubleshooting
+                </a>
+              </li>
+              <li>
+                <a href="https://docs.pancakeswap.finance/get-started">Guides</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>Developers</h4>
+            <ul className="menu-ul">
+              <li><a href="https://github.com/pancakeswap">Github</a></li>
+              <li><a href="https://docs.pancakeswap.finance/">Documentation</a></li>
+              <li><a href="https://docs.pancakeswap.finance/code/bug-bounty">Bug Bounty</a></li>
+              <li><a href="https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited">Audits</a></li>
+              <li><a href="https://docs.pancakeswap.finance/hiring/become-a-chef">Careers</a></li>
+            </ul>
+          </div>
+          <div className="flex2 logoFlex">
           <h1>
             <img src={logo} alt="logo"></img>
           </h1>
-        </div>
-      </section>
-      <section className="bottom-footer">
+          </div>
+        </section>
+        <section className="bottom-footer">
         <ul className="menu-ul">
           <li className="cursor">
             <img src={twitter} alt="logo twitter"></img>
@@ -98,21 +98,23 @@ export function Footer() {
           <li className="cursor"><img src={discord} alt="logo discord"></img></li>
           <li className="cursor"><img src={medium} alt="logo medium"></img></li>
         </ul>
-      </section>
-      <hr className="line"></hr>
-      <section className="bottom-footer-2">
-        <div className="mqflex">
-          <Checkbox/>
-          <LanguageSelector />
-        </div>
-        <div className="flex2">
+        </section>
+        <hr className="line"></hr>
+        <section className="bottom-footer-2">
+          <div className="mqflex">
+            <Checkbox/>
+            <LanguageSelector />
+          </div>
+          <div className="flex2 btnFlex">
          
-          <p>$4.229</p>
-          <a href="/" className="link-aquagreen">
-            <button className="btn-footer button-aquagreen ">Buy CAKE</button>
-          </a>
-        </div>
-      </section>
+            <p>$4.229</p>
+            <a href="/" className="link-aquagreen">
+              <button className="btn-footer button-aquagreen ">Buy CAKE</button>
+            </a>
+          </div>
+        
+        </section>
+        <hr className="line2"></hr>
       </section>
     </footer>
   );
