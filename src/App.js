30e743eccf019f27b0457576fcf3bc47d292b4fa
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-11-03 11:14:52 +0100

    fix: prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import Navbar from "./components/navbar/Navbar";
-import Trade from "./pages/Trade"
-import Earn from "./pages/Earn"
-import Win from "./pages/Win"
-import Home from "./pages/Home";
-import NoPage from "./pages/NoPage";
-import "./App.css"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Layout from "./pages/Layout";
-import { ButtonAG, ButtonOB, ButtonST, ButtonOSvg } from "./components/buttons/Buttons";
-=======
 import {Trade} from "./pages/trade/Trade";
 import {Earn} from "./pages/earn/Earn";
 import {Win} from "./pages/win/Win";
@@ -20,18 +8,11 @@ import "../src/main.scss";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {Layout} from "./pages/Layout";
 import {Nft} from "./pages/nft/Nft";
->>>>>>> 402726ad0be2e3
 
 export function App() {
     return (
         <div>
-<<<<<<< HEAD
-
-
-                {/* <Router>
-=======
             <Router>
->>>>>>> 402726ad0be2e3
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
@@ -43,20 +24,7 @@ export function App() {
                         <Route path="*" element={<NoPage />} />
                     </Route>
                 </Routes>
-<<<<<<< HEAD
-                </Router> */}
-
-               <ButtonAG/>
-               <ButtonOB/>
-               <ButtonST/>
-               <ButtonOSvg/>
-               
-
-
-            
-=======
             </Router>
->>>>>>> 402726ad0be2e3
         </div>
     );
 }
