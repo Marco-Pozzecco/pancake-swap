03f412fc39d198e82108781e38b9c0b8271a8cb3
Milton Mariani <miltonmariani7@gmail.com>
2022-11-14 11:40:12 +0100

    cleaning code

--- src/layouts/home-main-banner/MainBanner.js
+++ src/layouts/home-main-banner/MainBanner.js
@@ -5,51 +5,16 @@ import "./_home-main-banner.scss"
 
 export function MainBanner() {
 
-
-// const [ slides, setSlides ] = useState('a')
-
-
-
-// const visibilityA = () => {
-//     setSlides('a')
-// }
-
-// const visibilityB = () => {
-//     setSlides('b')
-
-// }
-
-// const visibilityC = () => {
-//     setSlides('c')
-// }
-
-
-    // useEffect(() => {
-    //     setSlides()
-    // }, [slides])
-
-
   return (
 
     <section className="main-banner-container">          
         <div className="main-banner-sub-container">
-
- {/* {slides === 'a' && <BannerA /> }
- {slides === 'b' && <BannerB /> }
- {slides === 'c' && <BannerC /> } */}
-
-
-    <BannerA />
-    <BannerB />
-    <BannerC />
- 
-
-{/* <MainBannerSwiper a={visibilityA} b={visibilityB} c={visibilityC}/> */}
-    <MainBannerSwiper />
-
-
-</div>
-    </section>
+            <BannerA />
+            <BannerB />
+            <BannerC />
+            <MainBannerSwiper />
+        </div>
+            </section>
 
     )
 }
--- src/layouts/home-main-banner/MainBannerSwiper.js
+++ src/layouts/home-main-banner/MainBannerSwiper.js
@@ -2,13 +2,12 @@ import React from 'react'
 import "./_home-main-banner.scss"
 
 
-export function MainBannerSwiper({a, b, c}) {
-
+export function MainBannerSwiper() {
   return (
     <div className="swiper-slide"  >
-    <div className="swiper swpr-a" onClick={a}></div>
-    <div className="swiper swpr-b" onClick={b}></div>
-    <div className="swiper swpr-c" onClick={c}></div>
+    <div className="swiper swpr-a" ></div>
+    <div className="swiper swpr-b" ></div>
+    <div className="swiper swpr-c" ></div>
 </div>
   )
 }
