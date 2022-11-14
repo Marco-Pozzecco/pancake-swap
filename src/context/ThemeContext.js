36099a0084c2671da096f10838b638ad22cb049d
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-11-14 15:41:35 +0100

    aggiunta documentazione ThemeContext

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 
+// Utilizzare la sintassi const {theme} = useContext(ThemeContext) per ottenere in stringa il tema corrente
+
 const context = {
     theme: '',
     setTheme: () => {}
