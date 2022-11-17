26c950687ffa6fd30f53a784e30c2d7b00db5234
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-11-17 18:25:40 +0100

    setup responsive layout for desktop mq

--- src/layouts/perpetual-grid/PerpetualGrid.js
+++ src/layouts/perpetual-grid/PerpetualGrid.js
@@ -7,14 +7,28 @@ import { OrderForm } from "../../components/finance/OrderForm";
 import { FinancialTrades } from "../../components/finance/FinancialTrades"
 import { FinancialIntrumentSelector } from "../../components/finance/FinancialInstrumentSelector";
 import { FinancialGraph } from "../../components/finance/FinancialGraph"
+import { FinancialMarket } from "../../components/finance/FinancialMarket";
+import { FinancialAssets } from "../../components/finance/FinancialAssets";
+import { FinancialDepth } from "../../components/finance/FinancialDepth";
+import { FinancialOrders } from "../../components/finance/FinancialOrders";
 
 export function PerpetualGrid () {
     const ResponsiveGridLayout = WidthProvider(Responsive);
 
     const layout = {
-        desktop: {
-
-        },
+        desktop: [
+            { x: 0, y: 0, w: 18, h: 3, i: 'favorite-fi'},
+            { x: 0, y: 3, w: 18, h: 4, i: 'financial-selector'},
+            { x:18, y: 0, w: 3, h: 20, i: 'order-book'},
+            { x:22, y: 0, w: 3, h: 18, i: 'order-form'},
+            { x:22, y: 0, w: 3, h: 12, i: 'margin-ratio'},
+            { x: 0, y: 7, w: 4, h: 28, i: 'market'},
+            { x:18, y: 21, w: 3, h: 15, i: 'financial-trades'},
+            { x:4, y: 7, w: 14, h: 28, i: 'financial-graph'},
+            { x:21, y: 36, w: 3, h: 23, i: 'financial-assets'},
+            { x: 0, y: 35, w: 21, h: 8, i: 'depth'},
+            { x: 0, y: 43, w: 21, h: 10, i: 'orders'}
+        ],
         tablet: {
 
         },
@@ -27,36 +41,53 @@ export function PerpetualGrid () {
         <>
             <ResponsiveGridLayout
                 className="layout"
+                layouts={layout}
                 rowHeight={10}
                 breakpoints={{ desktop: 1200, tablet: 996, mobile: 768 }}
-                cols={{ desktop: 12, tablet: 10, mobile: 6 }}
+                cols={{ desktop: 24, tablet: 24, mobile: 6 }}
             >
-                <div key="favorite-fi" data-grid={{x:0, y:0, w: 8, h:3}}>
+                <div key="favorite-fi">
                     <FavoriteFinancialInstruments />
                 </div>
 
-                <div key="order-form" data-grid={{x:10, y:0, w: 2, h:2}}>
+                <div key="order-form">
                     <OrderForm />
                 </div>
                 
-                <div key="order-book" data-grid={{x:8, y:0, w: 2, h:30}}>
+                <div key="order-book">
                     <OrderBook />
                 </div>
 
-                <div key="margin-ratio" data-grid={{x:0, y:0, w: 2, h:2}}>
+                <div key="margin-ratio">
                     <MarginRatio />
                 </div>
 
-                <div key="financial-trades" data-grid={{x:0, y:0, w: 2, h:2}}>
+                <div key="financial-trades">
                     <FinancialTrades />
                 </div>
 
-                <div key="financial-selector" data-grid={{x:0, y:0, w: 2, h:2}}>
+                <div key="financial-selector">
                     <FinancialIntrumentSelector />
                 </div>
 
-                <div key="financial-graph" data-grid={{x:0, y:0, w:6, h:10}}>
+                <div key="financial-graph">
                     <FinancialGraph />
+                </div>
+
+                <div key="market">
+                    <FinancialMarket />
+                </div>
+
+                <div key="financial-assets">
+                    <FinancialAssets />
+                </div>
+
+                <div key="depth">
+                    <FinancialDepth />
+                </div>
+
+                <div key="orders">
+                    <FinancialOrders />
                 </div>
                 
             </ResponsiveGridLayout> 
