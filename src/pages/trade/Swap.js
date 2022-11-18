ea59f5e1cd71512cac1bd287d781618bcb032b81
giobruno <giobruno@me.com>
2022-11-18 17:20:16 +0100

    swap page and css

--- src/pages/trade/Swap.js
+++ src/pages/trade/Swap.js
@@ -1,14 +1,108 @@
-import React from 'react'
-import Subnav from '../../components/subnav/Subnav';
+import React, { useState } from "react";
+import "./swap.scss";
+import Subnav from "../../components/subnav/Subnav";
+import barChart from "../../resources/limit/barIcon.svg";
+import historyIcon from "../../resources/limit/history.svg";
+import doubleArrow from "../../resources/limit/doubleArrow.svg";
+import bunnyLogo from "../../resources/home/navbar/bunny-icon-round.svg";
+import copied from "../../resources/limit/copied.svg";
+import arrowLimit from "../../resources/limit/arrowLimit.svg";
+import arrow from "../../resources/home/navbar/arrow-down.svg";
+import helpBunny from "../../resources/limit/help.png";
 
+import ModalConnectWallet from "../../components/modal-connect-wallet/ModalConnectWallet";
+import { ConnectWalletBtn } from "../../components/buttons/ConnectWalletBtn";
 
-export function Swap(){
+export function Swap() {
+  const [openModalWallet, setOpenModalWallet] = useState(false);
+
   return (
-    <div>
-      <Subnav elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}/> 
-      <h1 className='provaH1'>Swap</h1>
+    <div className="swap-box">
+      <Subnav
+        elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}
+      />
 
-      
+      <div className="jskBUs">
+        <section className="col-2">
+          <div className="ConverterCard ">
+            <div className="headerCard">
+              <div className="headerRow">
+                <img src={barChart} alt="history icon"></img>
+                <h3 className="header">LIMIT</h3>
+                <img src={historyIcon} alt="history icon"></img>
+              </div>
+              <p className="pg-tx">
+                Place a limit order to trade at a set price
+              </p>
+            </div>
+
+            <div className="ext-bottomCard">
+              <div className="bottomCard">
+                <div className="input1Converter">
+                  <div className="fx-inline switchCryptoBtn">
+                    <img src={bunnyLogo} alt="history icon"></img>CAKE
+                    <img className="fa-svg-icon" src={arrow} alt="arrow icon" />
+                    <img
+                      className="fa-svg-icon"
+                      src={copied}
+                      alt="arrow icon"
+                    />
+                  </div>
+                  <input placeholder="0.0" className="convertInput"></input>
+                </div>
+                <div className="flex">
+                  <button className="arrrowBtn">
+                    <img src={arrowLimit} alt="history icon"></img>
+                  </button>
+                </div>
+                <div className="input1Converter ">
+                  <div className="fx-inline switchCryptoBtn">
+                    <img src={bunnyLogo} alt="history icon"></img>BTCB
+                    <img className="fa-svg-icon" src={arrow} alt="arrow icon" />
+                    <img
+                      className="fa-svg-icon"
+                      src={copied}
+                      alt="arrow icon"
+                    />
+                  </div>
+                  <input placeholder="0.0" className="convertInput"></input>
+                </div>
+                <div className="fx-inline">
+                  <p>Price</p>
+                  <p>Market</p>
+                </div>
+                <input className="emptyDiv"></input>
+                <div className="rowAfterEmptyDiv">
+                  <p>BTCB</p>
+                  <p>per</p>
+                  <p>CAKE</p>
+                </div>
+              </div>
+              <ConnectWalletBtn
+                type="fullButton button-aqg"
+                action={() => setOpenModalWallet(true)}
+              />
+              <p className="var-text-color">
+                Powered by <strong>Gelato</strong>
+              </p>
+            </div>
+          </div>
+        </section>
+        <ModalConnectWallet
+          open={openModalWallet}
+          onClose={() => setOpenModalWallet(false)}
+        />
+        <div className="bottom-page-swap">
+          <p>Bridge assets to BNB Chain</p>
+
+          <div className="extBox-bunnyHelp">
+            <div className="emptyDiv-helpBunny"></div>
+            <div className="helpBunny">
+              <img src={helpBunny} alt="history icon"></img>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/pages/trade/limit/Limit.js
+++ src/pages/trade/limit/Limit.js
@@ -1,151 +1,183 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Subnav from "../../../components/subnav/Subnav";
-import './limit.scss'
+import "./limit.scss";
 import ModalConnectWallet from "../../../components/modal-connect-wallet/ModalConnectWallet";
 import { ConnectWalletBtn } from "../../../components/buttons/ConnectWalletBtn";
 import arrow from "../../../resources/home/navbar/arrow-down.svg";
-import barChart from '../../../resources/limit/barIcon.svg'
-import historyIcon from '../../../resources/limit/history.svg'
-import arrowLimit from '../../../resources/limit/arrowLimit.svg'
-import doubleArrow from '../../../resources/limit/doubleArrow.svg'
-import bunnyLogo from '../../../resources/home/navbar/bunny-icon-round.svg'
-import copied from '../../../resources/limit/copied.svg'
-import bunnyCard from '../../../resources/limit/imgBunnyBottmCard.svg'
-import bunnyCardLighTheme from '../../../resources/limit/bunnyBtmCardLightTheme.svg'
-import leftArrow from '../../../resources/limit/leftArrow.svg'
-import rightArrow from '../../../resources/limit/rightArrow.svg'
-import helpBunny from '../../../resources/limit/help.png'
+import barChart from "../../../resources/limit/barIcon.svg";
+import historyIcon from "../../../resources/limit/history.svg";
+import arrowLimit from "../../../resources/limit/arrowLimit.svg";
+import doubleArrow from "../../../resources/limit/doubleArrow.svg";
+import bunnyLogo from "../../../resources/home/navbar/bunny-icon-round.svg";
+import copied from "../../../resources/limit/copied.svg";
+import bunnyCard from "../../../resources/limit/imgBunnyBottmCard.svg";
+import bunnyCardLighTheme from "../../../resources/limit/bunnyBtmCardLightTheme.svg";
+import leftArrow from "../../../resources/limit/leftArrow.svg";
+import rightArrow from "../../../resources/limit/rightArrow.svg";
+import helpBunny from "../../../resources/limit/help.png";
 
 export function Limit() {
   const [openModalWallet, setOpenModalWallet] = useState(false);
   const [toggleState, setTab] = useState(1);
- 
 
   const toggleTab = (index) => {
     setTab(index);
-    console.log(index)
+    console.log(index);
   };
 
   return (
-    <div className='body-limit'>
-       <Subnav elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}/> 
+    <div className="body-limit">
+      <Subnav
+        elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}
+      />
 
-      <div className='limit-external-box'>
-      <section className='col-1'>
-        <div className='fakeGraph'>
-        </div>
+      <div className="limit-external-box">
+        <section className="col-1">
+          <div className="fakeGraph"></div>
 
-        <div className='col1-bottom-card'>
-          <div className="bloc-tabs-bottom-card">
-                  <div className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-                onClick={() => toggleTab(1)} >
-                  Open Orders</div>
-                  <div className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-                  onClick={() => toggleTab(2)} >
-                    Order History</div>
-                  
-          </div>
-          <div className='container-bottom-card'>      
-            
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className='th-table'>
-                      <div>From</div>
-                    </th>
-                    <th className='th-table'>
-                      <div>To</div>
-                    </th>
-                    <th className='th-table'>
-                      <div>Limit Price</div>
-                    </th>
-                    <th className='th-table'>
-                      <div>Status</div>
-                    </th>
-                  </tr>
-
-                </thead>     
-              </table>
-
-              <div className="content-bottom-card">
-                  <img className="" src={bunnyCard} alt='wallet'/>
-                  <p className={toggleState === 1 ? " switchTxt active-switchImg" : "switchTxt"}>No Open orders</p>
-                  <p className={toggleState === 2 ? " switchTxt active-switchImg" : "switchTxt"}>No Order History</p>
-
-              </div>
-              <div className='card-bottom-row'>
-                <img className="switchImg" src={leftArrow} alt='wallet'/>
-                <p>Page 1 of 1</p>
-                <img className="switchImg" src={rightArrow} alt='wallet'/>
-              </div>
-              
-            
-          </div>
-        </div>
-
-      </section>
-      <section className='col-2'>
-        <div className="ConverterCard ">
-          <div className="headerCard">
-              <div className='headerRow'>
-                        <img src={barChart} alt='history icon'></img>
-                          <h3 className="header">LIMIT</h3>
-                          <img src={historyIcon} alt='history icon'></img>
-                          
-              </div>
-              <p className='pg-tx'>Place a limit order to trade at a set price</p>
-          </div>
-                    
-                <div className='ext-bottomCard'>
-                  <div className='bottomCard'>
-                      <div className="input1Converter">
-                          <div className='fx-inline switchCryptoBtn'><img src={bunnyLogo} alt='history icon'>
-                            </img>CAKE<img className="fa-svg-icon" src={arrow} alt="arrow icon"/>
-                            <img className="fa-svg-icon" src={copied} alt="arrow icon"/>
-                          </div>
-                          <input placeholder='0.0' className='convertInput'></input>
-                      </div>
-                      <div className='flex'> 
-                        <button className='arrrowBtn'><img src={arrowLimit} alt='history icon'></img></button>
-                      </div>
-                      <div className="input1Converter ">
-                          <div className='fx-inline switchCryptoBtn'><img src={bunnyLogo} alt='history icon'></img>BTCB
-                          <img className="fa-svg-icon" src={arrow} alt="arrow icon"/>
-                          <img className="fa-svg-icon" src={copied} alt="arrow icon"/></div>
-                          <input placeholder='0.0'  className='convertInput'></input>
-                      </div>
-                      <div className='fx-inline'>
-                        <p>Price</p>
-                        <p>Market</p>
-                      </div>
-                      <input className='emptyDiv'>
-                      </input>
-                      <div className='rowAfterEmptyDiv'>
-                        <p>BTCB</p>
-                        <p>CAKE</p>
-                      </div>
-                    </div>
-                    <ConnectWalletBtn type="fullButton button-aquagreen" action={() => setOpenModalWallet(true)} />
-                    <p>Powered by <span className='var-text-color'>Gelato</span></p>
+          <div className="col1-btm-page">
+            <div className="col1-bottom-card">
+              <div className="bloc-tabs-bottom-card">
+                <div
+                  className={toggleState === 1 ? "tabs2 active-tabs" : "tabs2"}
+                  onClick={() => toggleTab(1)}
+                >
+                  Open Orders
                 </div>
-                
-                
-              </div>
-              <div className='extBox-bunnyHelp'>
-                <div className='emptyDiv-helpBunny'></div>
-                <div className='helpBunny'>
-                  <img src={helpBunny} alt='history icon'></img>
+                <div
+                  className={toggleState === 2 ? "tabs2 active-tabs" : "tabs2"}
+                  onClick={() => toggleTab(2)}
+                >
+                  Order History
                 </div>
-
               </div>
-              
-        
-      </section>
-      <ModalConnectWallet open={openModalWallet}
-                  onClose={() => setOpenModalWallet(false)} />
-      
-    
+              <div className="container-bottom-card">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th className="th-table">
+                        <div>From</div>
+                      </th>
+                      <th className="th-table">
+                        <div>To</div>
+                      </th>
+                      <th className="th-table">
+                        <div>Limit Price</div>
+                      </th>
+                      <th className="th-table">
+                        <div>Status</div>
+                      </th>
+                    </tr>
+                  </thead>
+                </table>
+
+                <div className="content-bottom-card">
+                  <img className="" src={bunnyCard} alt="wallet" />
+                  <p
+                    className={
+                      toggleState === 1
+                        ? " switchTxt active-switchImg"
+                        : "switchTxt"
+                    }
+                  >
+                    No Open orders
+                  </p>
+                  <p
+                    className={
+                      toggleState === 2
+                        ? " switchTxt active-switchImg"
+                        : "switchTxt"
+                    }
+                  >
+                    No Order History
+                  </p>
+                </div>
+                <div className="card-bottom-row">
+                  <img className="switchImg" src={leftArrow} alt="wallet" />
+                  <p>Page 1 of 1</p>
+                  <img className="switchImg" src={rightArrow} alt="wallet" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="col-2">
+          <div className="ConverterCard ">
+            <div className="headerCard">
+              <div className="headerRow">
+                <img src={barChart} alt="history icon"></img>
+                <h3 className="header">LIMIT</h3>
+                <img src={historyIcon} alt="history icon"></img>
+              </div>
+              <p className="pg-tx">
+                Place a limit order to trade at a set price
+              </p>
+            </div>
+
+            <div className="ext-bottomCard">
+              <div className="bottomCard">
+                <div className="input1Converter">
+                  <div className="fx-inline switchCryptoBtn">
+                    <img src={bunnyLogo} alt="history icon"></img>CAKE
+                    <img className="fa-svg-icon" src={arrow} alt="arrow icon" />
+                    <img
+                      className="fa-svg-icon"
+                      src={copied}
+                      alt="arrow icon"
+                    />
+                  </div>
+                  <input placeholder="0.0" className="convertInput"></input>
+                </div>
+                <div className="flex">
+                  <button className="arrrowBtn">
+                    <img src={arrowLimit} alt="history icon"></img>
+                  </button>
+                </div>
+                <div className="input1Converter ">
+                  <div className="fx-inline switchCryptoBtn">
+                    <img src={bunnyLogo} alt="history icon"></img>BTCB
+                    <img className="fa-svg-icon" src={arrow} alt="arrow icon" />
+                    <img
+                      className="fa-svg-icon"
+                      src={copied}
+                      alt="arrow icon"
+                    />
+                  </div>
+                  <input placeholder="0.0" className="convertInput"></input>
+                </div>
+                <div className="fx-inline">
+                  <p>Price</p>
+                  <p>Market</p>
+                </div>
+                <input className="emptyDiv"></input>
+                <div className="rowAfterEmptyDiv">
+                  <p>BTCB</p>
+                  <p>per</p>
+                  <p>CAKE</p>
+                </div>
+              </div>
+              <ConnectWalletBtn
+                type="fullButton button-aqg"
+                action={() => setOpenModalWallet(true)}
+              />
+              <p className="var-text-color">
+                Powered by <strong>Gelato</strong>
+              </p>
+            </div>
+          </div>
+          <div className="extBox-bunnyHelp">
+            <div className="emptyDiv-helpBunny"></div>
+            <div className="helpBunny">
+              <img src={helpBunny} alt="history icon"></img>
+            </div>
+          </div>
+        </section>
+        <ModalConnectWallet
+          open={openModalWallet}
+          onClose={() => setOpenModalWallet(false)}
+        />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
