a9c73868a1d0e46cbf0681a06040d7a00c2baa4c
giobruno <giobruno@me.com>
2022-11-20 09:21:12 +0100

    page liquidity

--- src/pages/trade/liquidity/Liquidity.js
+++ src/pages/trade/liquidity/Liquidity.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 import Subnav from "../../../components/subnav/Subnav";
+import "./liquidity.scss";
+import helpBunny from "../../../resources/limit/help.png";
 
 export function Liquidity() {
   return (
-    <div>
-      <Subnav elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]}/> 
-      <h1 className='provaH1'>Liquidity</h1>
+    <div className="liquidity-ext-page">
+      <Subnav elements={["Swap", "Limit", "Liquidity", "Perpetual", "Bridge"]} />
+
+      <div className="liquidity-box-page"></div>
+      <div className="bottom-page-swap">
+        <p>Bridge assets to BNB Chain</p>
+
+        <div className="extBox-bunnyHelp">
+          <div className="emptyDiv-helpBunny"></div>
+          <div className="helpBunny">
+            <img src={helpBunny} alt="history icon"></img>
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
