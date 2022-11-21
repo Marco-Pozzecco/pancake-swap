eb94da5df86e019d1b82100effcc214c54b346f4
Milton Mariani <miltonmariani7@gmail.com>
2022-11-21 17:47:43 +0100

    stAdding useCountdown in MainBannerCountdown

--- src/layouts/home-main-banner/MainBannerCounterLottery.js
+++ src/layouts/home-main-banner/MainBannerCounterLottery.js
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
 import "./_home-main-banner.scss";
+import { useCountdown } from "../../../src/hooks/useCountdown";
 
 //INIZIO VECCHIO--------------------------------------------------------
 
-export function MainBannerCounterLottery() {
-  // let countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
-
-  // let x = setInterval(function () {
-  //   let now = new Date().getTime();
-
-  //   let distance = countDownDate - now;
-
-  //   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  //   let hours = Math.floor(
-  //     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  //   );
-  //   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  //   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  //   document.getElementById("counterLottery").innerHTML =
-  //     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  //   if (distance < 0) {
-  //     clearInterval(x);
-  //     document.getElementById("counterLottery").innerHTML = "EXPIRED";
-  //   }
-  // }, 1000);
-
-  // FINE VECCHIO-----------------------------------------------------------------------------
+export function MainBannerCounterLottery({ days, hours, minutes, seconds }) {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
   return (
     <div className="mb-counter-lottery">
@@ -39,3 +17,5 @@ export function MainBannerCounterLottery() {
     </div>
   );
 }
+
+// METTERE LE PROPS!!!!  https://blog.greenroots.info/how-to-create-a-countdown-timer-using-react-hooks#heading-the-countdown-app
