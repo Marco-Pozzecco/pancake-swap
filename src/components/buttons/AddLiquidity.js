6e708ed839293f96ee9cd8c8a2b33e6c2f04c6f6
giobruno <giobruno@me.com>
2022-11-22 18:11:21 +0100

    add icon

--- src/components/buttons/AddLiquidity.js
+++ src/components/buttons/AddLiquidity.js
@@ -3,9 +3,5 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 
 export function AddLiquidityBtn(props) {
-  const myStyle = {
-    color: "black",
-  };
-
-  return <Button svg={props.svg} type={props.type} action={props.action} text="Add Liquidity" style={myStyle} />;
+  return <Button svg={props.svg} type={props.type} action={props.action} text="Add Liquidity" />;
 }
