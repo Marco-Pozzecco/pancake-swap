22602ec4e70ee8c09fcd435f5bb6f66a977e098c
Milton Mariani <miltonmariani7@gmail.com>
2022-11-25 19:20:17 +0100

    initializing EarnPool component

---
+++ src/layouts/earn-pool/EarnPools.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./_earn-pool.scss";
+
+export function EarnPools() {
+  return <section className="earn-pool-section">AAA</section>;
+}
--- src/pages/earn/pools/Pools.js
+++ src/pages/earn/pools/Pools.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Subnav from '../../../components/subnav/Subnav';
+import React from "react";
+import Subnav from "../../../components/subnav/Subnav";
+import { EarnPools } from "../../../layouts/earn-pool/EarnPools";
 
 export function Pools() {
   return (
     <div>
-        <Subnav elements={["Farms", "Pools"]} />
-        <h1 className='provaH1'>Pool</h1>
+      <Subnav elements={["Farms", "Pools"]} />
+      {/* <h1 className="provaH1">Pool</h1> */}
 
+      <EarnPools />
     </div>
-  )
+  );
 }
