68bfbc3d6296e4d11e0f6e33ba506e3725d06176
Marco Pozzecco <pozzecco.marco@gmail.com>
2022-12-03 11:28:11 +0100

    fix typo in fetchModule

--- src/script/fetchModule.js
+++ src/script/fetchModule.js
@@ -6,7 +6,7 @@ export class fetchModule {
     const today = Math.round(Date.now() / 1000); // today in seconds UNIX format
 
     const response = await fetch(
-      `https://api.coingecko.com/api/v3/coins/${coin}/market_chart/range?vs_currency=${vs_currency}&from=${start_date}&to=${today}`
+      `${BaseURL}coins/${coin}/market_chart/range?vs_currency=${vs_currency}&from=${start_date}&to=${today}`
     ).then((res) => res.json());
 
     return response;
