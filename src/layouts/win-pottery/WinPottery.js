32d554167574aa023bb6668f9faff720a299cc9d
Milton Mariani <miltonmariani7@gmail.com>
2022-12-06 21:32:02 +0100

    create ref-win-pottery branch

--- src/layouts/win-pottery/WinPottery.js
+++ src/layouts/win-pottery/WinPottery.js
@@ -12,14 +12,7 @@ export function WinPottery() {
   return (
     <div className="wp-main-container">
       <h1>WinPottery.js</h1>
-      <h1>NON FARE MODIFICHE QUI!</h1>
-      <h3>spostarsi sul ramo: </h3>
-      <p>↓↓↓↓</p>
-
-      <p>↓↓↓↓↓↓↓↓</p>
-      <p>↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓</p>
-      <h1>ref-win-pottery</h1>
-      <p>↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑</p>
+      <h1>FARE MODIFICHE QUI! RAMO GIUSTO!</h1>
       <ToBeWon />
       <CurrentPrizePot />
       <FinishedRoundWP />
