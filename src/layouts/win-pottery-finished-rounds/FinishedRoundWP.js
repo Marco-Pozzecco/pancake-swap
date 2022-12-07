95d3f1feb1a9ad5078177b92eee7095ab9dff751
Milton Mariani <miltonmariani7@gmail.com>
2022-12-07 14:18:16 +0100

    Add FinishedRounds section basic

--- src/layouts/win-pottery-finished-rounds/FinishedRoundWP.js
+++ src/layouts/win-pottery-finished-rounds/FinishedRoundWP.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./_finished-rounds-wp.scss";
 
 export function FinishedRoundWP() {
-  return <section className="fr-wp-main-container">FinishedRoundWP</section>;
+  return (
+    <section className="fr-wp-main-container">
+      <div className="fr-wp-sub-container">
+        <div className="fr-wp-title-wrapper">
+          <h2 className="fr-wp-title">Finished Rounds</h2>
+        </div>
+        <div className="fr-wp-card">CARD</div>
+      </div>
+    </section>
+  );
 }
