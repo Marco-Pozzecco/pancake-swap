9d2fbdb09f70e1f83d89978495fdfac3be0a6478
Milton Mariani <miltonmariani7@gmail.com>
2022-12-07 13:51:13 +0100

    Add CurrentPrizePot section and fixed width/max-width in ToBeWon

--- src/layouts/win-pottery-current-prize-pot/CurrentPrizePot.js
+++ src/layouts/win-pottery-current-prize-pot/CurrentPrizePot.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./_current-prize-pot.scss";
 
 export function CurrentPrizePot() {
-  return <section className="ccp-main-container">CurrentPrizePot</section>;
+  return (
+    <section className="ccp-main-container">
+      <div className="ccp-sub-container">
+        <div className="ccp-titles">
+          <div className="ccp-title-a ccp-title">Current Prize Pot</div>
+          <div className="ccp-amount">$17,721</div>
+          <div className="ccp-title-b ccp-title">
+            Stake to get your tickets NOW
+          </div>
+        </div>
+        <div className="ccp-content-wrapper">
+          <div className="ccp-card-container">CARD</div>
+          <div className="ccp-image-container">
+            <img src="https://pancakeswap.finance/images/pottery/honeypot.png" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
