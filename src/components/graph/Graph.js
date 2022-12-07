9ea0f640d01b3de32c22b796432dc891a1d4d772
giobruno <giobruno@me.com>
2022-12-07 15:23:57 +0100

    small fix

--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 // API
 import { CoinGeckoAPI } from "../../script/CoinGeckoAPI";
 
-
 export function Graph(props) {
   // API
   const API = new CoinGeckoAPI();
@@ -13,9 +12,9 @@ export function Graph(props) {
     async function fetchData() {
       const days = [1, 7, 30, 365];
       const timeframe = ["24H", "1W", "1M", "1Y"];
-      
+
       const response = await API.fetchHystoricalData(days[timeframe.indexOf(props.timeframe)], undefined, props.financialInstrument[0], props.financialInstrument[1]);
-      console.log(props.timeframe, response)
+      console.log(props.timeframe, response);
 
       await createChart(response);
     }
@@ -23,21 +22,21 @@ export function Graph(props) {
     // API f call
     setTimeout(() => {
       fetchData();
-    }, 500)
-    
+    }, 500);
+
     let graph;
 
-    async function createChart (data) {
+    async function createChart(data) {
       // query div graph
-      const graphEl = document.querySelector('.graph');
+      const graphEl = document.querySelector(".graph");
       // distruzione heading
       const headingEl = document.getElementById(`${props.id}-title`);
       graphEl.removeChild(headingEl);
 
       // creazione grafico
       const canvasEl = document.createElement("canvas");
-      
-      canvasEl.setAttribute('id', props.id)
+
+      canvasEl.setAttribute("id", props.id);
       graphEl.appendChild(canvasEl);
 
       graph = new Chart(document.getElementById(props.id), {
@@ -79,15 +78,15 @@ export function Graph(props) {
                 //drawTicks: true,
               },
             },
-            // y: {
-            //   beginAtZero: true,
-            //   max: 100,
-            //   steps: 3,
-            //   display: false,
-            //   grid: {
-            //     display: false,
-            //   },
-            // },
+            y: {
+              //beginAtZero: true,
+              //max: 100,
+              //steps: 3,
+              display: false,
+              grid: {
+                display: false,
+              },
+            },
           },
           plugins: {
             legend: {
@@ -96,67 +95,65 @@ export function Graph(props) {
           },
         },
       });
-      
+
       graph.render();
     }
 
     // Creates loading.. heading while loading chart
     const graphEl = document.querySelector(".graph");
     const headingEl = document.createElement("h1");
-    headingEl.setAttribute('id', `${props.id}-title`);
-    headingEl.textContent = "Loading.."
+    headingEl.setAttribute("id", `${props.id}-title`);
+    headingEl.textContent = "Loading..";
     graphEl.appendChild(headingEl);
-
 
     return () => {
       if (document.querySelector(`#${props.id}`)) {
         return graph.destroy();
       } else {
-        const graphEl = document.querySelector('.graph');
+        const graphEl = document.querySelector(".graph");
         const headingEl = document.getElementById(`${props.id}-title`);
         return graphEl.removeChild(headingEl);
       }
-    }
-  }, [props.financialInstrument, props.timeframe ]);
+    };
+  }, [props.financialInstrument, props.timeframe]);
 
-
-  return <div className="graph"></div>
+  return <div className="graph"></div>;
 }
 
 Graph.defaultProps = {
   responsive: true,
 };
 
-  // Basic configuration
-  // const config = {
-  //     type: 'line' | 'bar'
-  //     data: {}
-  //     options: {}
-  //     plugins: []
-  // }
+// Basic configuration
+// const config = {
+//     type: 'line' | 'bar'
+//     data: {}
+//     options: {}
+//     plugins: []
+// }
 
-  //Add vertical line
-  // Chart.defaults.LineWithLine = Chart.defaults.line;
-  // Chart.controllers.LineWithLine = Chart.controllers.line.extend({
-  //   draw: function (ease) {
-  //     Chart.controllers.line.prototype.draw.call(this, ease);
+//Add vertical line
+// Chart.defaults.LineWithLine = Chart.defaults.line;
+// Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+//   draw: function (ease) {
+//     Chart.controllers.line.prototype.draw.call(this, ease);
 
-  //     if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-  //       var activePoint = this.chart.tooltip._active[0],
-  //         ctx = this.chart.ctx,
-  //         x = activePoint.tooltipPosition().x,
-  //         topY = this.chart.scales["y-axis-0"].top,
-  //         bottomY = this.chart.scales["y-axis-0"].bottom;
+//     if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+//       var activePoint = this.chart.tooltip._active[0],
+//         ctx = this.chart.ctx,
+//         x = activePoint.tooltipPosition().x,
+//         topY = this.chart.scales["y-axis-0"].top,
+//         bottomY = this.chart.scales["y-axis-0"].bottom;
 
-  //       // draw line
-  //       ctx.save();
-  //       ctx.beginPath();
-  //       ctx.moveTo(x, topY);
-  //       ctx.lineTo(x, bottomY);
-  //       ctx.lineWidth = 4;
-  //       ctx.strokeStyle = "#757575";
-  //       ctx.stroke();
-  //       ctx.restore();
-  //     }
-  //   },
-  // });
+//       // draw line
+//       ctx.save();
+//       ctx.beginPath();
+//       ctx.moveTo(x, topY);
+//       ctx.lineTo(x, bottomY);
+//       ctx.lineWidth = 4;
+//       ctx.strokeStyle = "#757575";
+//       ctx.stroke();
+//       ctx.restore();
+//     }
+//   },
+// });
