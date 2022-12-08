e14df724e90850373bc02a05e1c5420c5863b0b4
Milton Mariani <miltonmariani7@gmail.com>
2022-12-08 21:35:11 +0100

    Add Rules.js basic placement

--- src/layouts/win-trading-competition-rules/RulesTC.js
+++ src/layouts/win-trading-competition-rules/RulesTC.js
@@ -2,5 +2,24 @@ import React from "react";
 import "./_rules-tc.scss";
 
 export function RulesTC() {
-  return <section className="r-tc-main-container">RulesTC</section>;
+  return (
+    <section className="r-tc-main-container">
+      <div className="r-tc-main-svg-container">
+        <div className="rtc-strip-svg-box">
+          Style svg e posizionamento gia fatti, INSERIRE SVG
+        </div>
+        <div className="rtc-main-svg-box-wrapper">IMMAGINE</div>
+      </div>
+      {/* -------------------------------------- */}
+      <div className="r-tc-sub-container">
+        <div className="rtc-card-box-wrapper">CARD</div>
+        <div className="rtc-description-box-wrapper">
+          <div className="rtc-description">INSERIRE TAG PER TITOLI E TESTO</div>
+          <div className="rtc-img-box-wp">
+            INSERIRE DIV CHE CONTERRA L'SVG E CENTRARLO
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/layouts/win-trading-competition-team-ranks/TeamRanksTC.js
+++ src/layouts/win-trading-competition-team-ranks/TeamRanksTC.js
@@ -4,21 +4,32 @@ import "./_team-ranks-tc.scss";
 export function TeamRanksTC() {
   return (
     <section className="tr-tc-main-container">
-      <div className="tr-tc-main-svg-container"> SVG Team Ranks</div>
+      <div className="tr-tc-main-svg-container">
+        <div className="trtc-strip-svg-box">
+          Style svg e posizionamento gia fatti, INSERIRE SVG
+        </div>
+        <div className="trtc-main-svg-box-wrapper">IMMAGINE</div>
+      </div>
       {/* --------------------------------------------------- */}
       <div className="tr-tc-sub-container">
         <div className="tr-tc-rank-box">
           <div className="tr-tc-podium-rank">
             <div className="trtc-pr-images-wrapper">immagine rank</div>
-            <div className="tr-tc-pr-data-wrapper">dati rank</div>
+            <div className="tr-tc-pr-data-wrapper">
+              dati rank - creare componente con props dei dati e inserirlo x 3
+            </div>
           </div>
-          <div className="tr-tc-partecipants">DIV & IMMAGINE</div>
+          <div className="tr-tc-partecipants">
+            <div className="trtc-ttb-elelement-wrapper">
+              Qui inserire in componente card da creare
+            </div>
+            <div className="trtc-ttb-img-wrapper">
+              QUI L IMMAGINE SPOSTATA IN BASSO(?)
+            </div>
+          </div>
         </div>
         {/* --- --- --- --- --- --- --- */}
-        <div className="tr-tc-top-traders-box">
-          <div className="trtc-ttb-elelement-wrapper">AAA</div>
-          <div className="trtc-ttb-img-wrapper">BBB</div>
-        </div>
+        <div className="tr-tc-top-traders-box"></div>
       </div>
     </section>
   );
