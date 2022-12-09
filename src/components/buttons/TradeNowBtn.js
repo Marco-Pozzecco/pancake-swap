03c7c4f6d58c11b744943cdff554e9d7d43bdb94
Milton Mariani <miltonmariani7@gmail.com>
2022-12-09 22:01:39 +0100

    Add Card less logic

--- src/components/buttons/TradeNowBtn.js
+++ src/components/buttons/TradeNowBtn.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from "./Button";
+import { Button } from "./Button";
 
 export function TradeNowBtn() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function openTradeNow() {
-        navigate('trade')
-    }
+  function openTradeNow() {
+    navigate("trade");
+  }
 
-    return <Button type="onlyBorderButton" action={openTradeNow} text="Trade Now" />;
+  return (
+    <Button type="onlyBorderButton" action={openTradeNow} text="Trade Now" />
+  );
 }
--- src/components/card/get-your-ticket-card/GetYourTicketCard.js
+++ src/components/card/get-your-ticket-card/GetYourTicketCard.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./_get-your-ticket-card.scss";
+import { Button } from "../../buttons/Button";
+import "../../buttons/_buttons.scss";
+import { FaAngleDown } from "react-icons/fa";
 
 export function GetYourTicketCard() {
   const [showDetails, setShowDetails] = useState(false);
-
-  const svgPath =
-    "M8.11997 9.29006L12 13.1701L15.88 9.29006C16.27 8.90006 16.9 8.90006 17.29 9.29006C17.68 9.68006 17.68 10.3101 17.29 10.7001L12.7 15.2901C12.31 15.6801 11.68 15.6801 11.29 15.2901L6.69997 10.7001C6.30997 10.3101 6.30997 9.68006 6.69997 9.29006C7.08997 8.91006 7.72997 8.90006 8.11997 9.29006Z";
+  // ------------------------------------------------------
 
   return (
     <section className="gytc-main-container">
@@ -22,22 +23,46 @@ export function GetYourTicketCard() {
         {/* ----BODY--------------------- */}
         <div className="gytc-body-container">
           <div className="gytc-body-sub-container">
-            <div className="gytc-sbi gytc-prize-pocket-box">
-              <div className="gytc-sub-sbi gytc-sbi-a1">Prize Pot </div>
-              <div className="gytc-sub-sbi gytc-sbi-a2">
-                <h2>~$98,441</h2>
-                <p>24,807 CAKE</p>
+            {/* --   --   --   --   --   --    */}
+            <div className="gytc-sbi gytcsbi-a ">
+              <div className="gytc-sbi-sub gytcbs-a ">
+                <h2>Prize Pot</h2>
+              </div>
+              <div className="gytc-sbi-sub gytcbs-b ">
+                <div className="gytcbs-amount gytcbs-amount-a">
+                  <span>$23,567</span>
+                </div>
+                <div className="gytcbs-amount gytcbs-amount-b">
+                  <span>$23,567 CAKE</span>
+                </div>
               </div>
             </div>
             {/* --   --   --   --   --   --    */}
-            <div className="gytc-sbi gytc-your-tickets-box">
-              {" "}
-              <div className="gytc-sub-sbi gytc-sbi-b1">Your tickets </div>
-              <div className="gytc-sub-sbi gytc-sbi-b2">
-                <button>button</button>
+            <div className="gytc-sbi gytcsbi-b">
+              <div className="gytc-sbi-sub gytcbs-a ">
+                <h2>Your tickets</h2>
+              </div>
+              <div className="gytc-sbi-sub gytcbs-b">
+                <Button
+                  className="gytcbs-b-button"
+                  type={"fullButton"}
+                  text="Buy Tickets"
+                  style={{ color: "var(--background-primary)" }}
+                  action={() => {}}
+                />
               </div>
             </div>
+            {/* --   --   --   --   --   --    */}
           </div>
+
+          {/* -------- */}
+          {/* -------- */}
+          {/* -------- */}
+          {/* -------- */}
+          {/* -------- */}
+          {/* -------- */}
+          {/* -------- */}
+
           <div
             // className="gytc-data"
             className={`hidden-section-body-row ${showDetails && "active"}`}
@@ -49,13 +74,21 @@ export function GetYourTicketCard() {
               display: showDetails ? "block" : "none",
             }}
           >
-            {/* <div className="gytc-sub-data aaa">ccc</div> */}
-            {/* <div className="gytc-sub-data bbb"></div> */}
+            aaa
           </div>
         </div>
+
         {/* --FOOTER------------------- */}
         <div className="gytc-footer-container">
-          <div className="gytc-footer-btn">
+          <Button
+            text="Details"
+            type={"noBorderButton"}
+            style={{ color: "rgb(114, 239, 255)", textDecoration: "none" }}
+            onClick={() => setShowDetails(!showDetails)}
+          >
+            <FaAngleDown />
+          </Button>
+          {/* <div className="gytc-footer-btn">
             Details
             <a href="#" onClick={() => setShowDetails(!showDetails)}>
               <svg width="32" height="32" viewBox="0 0 38 38">
@@ -64,7 +97,7 @@ export function GetYourTicketCard() {
                 </g>
               </svg>
             </a>
-          </div>
+          </div> */}
         </div>
         {/* -------------------------- */}
       </div>
