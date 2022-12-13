c7d9ab6268b9d1c88a20cc5a5331218d4b10f282
Milton Mariani <miltonmariani7@gmail.com>
2022-12-13 13:55:33 +0100

    Add FinishedRoundCardDatas in FinishedRoundCard and fixs, miss the counter/ round component at top-left, link in down-right

--- src/components/datas/finished-round-datas/FinishedRoundsDatas.js
+++ src/components/datas/finished-round-datas/FinishedRoundsDatas.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./_finished-rounds-datas.scss";
 
-export function FinishedRoundsDatas() {
+export function FinishedRoundsDatas(props) {
   return (
     <div className="frd-main-container">
-      <div className="frd-sub-container-a">
-        props.
-        <br />
-        img
-      </div>
+      <div className="frd-sub-container-a">{props.frd_datas_icon}</div>
+      {/* -------------------------------------------------------------------- */}
       <div className="frd-sub-container-b">
-        <div className="frd-sc-text frsct-a">PROPS.Text a</div>
-        <div className="frd-sc-text frsct-b">PROPS.Text b</div>
+        {props.frd_datas_code ? (
+          <div className="frd-sc-text frsct-a">{props.frd_datas_code}</div>
+        ) : (
+          <div className="frd-transparent-placeholder-datas">x</div>
+        )}
+        {/* -------------------------------------------------------------------- */}
+        {props.frd_datas_user ? (
+          <div className="frd-sc-text frsct-b">{props.frd_datas_user}</div>
+        ) : (
+          <div className="frd-transparent-placeholder-datas"></div>
+        )}
+        {/* -------------------------------------------------------------------- */}
       </div>
     </div>
   );
