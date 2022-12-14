7bd95da0b4c382a8ac27cdf6d7906426f70c91c1
Milton Mariani <miltonmariani7@gmail.com>
2022-12-14 16:13:08 +0100

    Doing RoundsInput.js

--- src/components/card/finished-round-card-lottery/win-lottery-finished-rounds-all-hystory/FinishedRoundCardLotteryAH.js
+++ src/components/card/finished-round-card-lottery/win-lottery-finished-rounds-all-hystory/FinishedRoundCardLotteryAH.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "../../../buttons/Button";
 import "./_finished-round-card-lottery-ah.scss";
 import { FaAngleDown } from "react-icons/fa";
+import { RoundsInput } from "../../../inpunts/round-input-with-counter/RoundsInput";
 
 export function FinishedRoundCardLotteryAH() {
   return (
@@ -10,10 +11,7 @@ export function FinishedRoundCardLotteryAH() {
         {/* --- HEADER --- */}
         <div className="frcl-ah-header">
           <div className="frcl-ahh-sub frcl-ahhs-a">
-            <div className="frcl-ahhs-a-1">
-              <h2>Round 746</h2>
-            </div>
-            <div className="frcl-ahhs-a-1">➡</div>
+            <RoundsInput />
           </div>
           <div className="frcl-ahh-sub frcl-ahhs-b">
             Drawn Dec 13, 2022, 1:00 PM
@@ -28,6 +26,11 @@ export function FinishedRoundCardLotteryAH() {
             <div className="frcl-ah-balls-wrapper">
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
+                {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-a">1</div>
+
+                {/* -- -- -- -- -- --  */}
+
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="sc-4ba21b47-0 IIbzK"
@@ -60,8 +63,6 @@ export function FinishedRoundCardLotteryAH() {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                {/* -- -- -- -- -- --  */}
-                {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
               </div>
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
@@ -99,6 +100,7 @@ export function FinishedRoundCardLotteryAH() {
                 </svg>
                 {/* -- -- -- -- -- --  */}
                 {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-b">9</div>
               </div>
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
@@ -136,6 +138,7 @@ export function FinishedRoundCardLotteryAH() {
                 </svg>
                 {/* -- -- -- -- -- --  */}
                 {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-c">1</div>
               </div>
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
@@ -173,6 +176,7 @@ export function FinishedRoundCardLotteryAH() {
                 </svg>
                 {/* -- -- -- -- -- --  */}
                 {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-d">1</div>
               </div>
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
@@ -210,6 +214,7 @@ export function FinishedRoundCardLotteryAH() {
                 </svg>
                 {/* -- -- -- -- -- --  */}
                 {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-e">2</div>
               </div>
               {/* -------------------------------- */}
               <div className="frcl-ah-ball-box">
@@ -247,6 +252,7 @@ export function FinishedRoundCardLotteryAH() {
                 </svg>
                 {/* -- -- -- -- -- --  */}
                 {/* // QUI MANCA DI AGGIUNGERE I DIV CON I NUMERI CHE CI SONO ALL INTENRO DELLE PALLE COLORATE */}
+                <div className="frcl-ahb-number-box frcl-ahb-nb-f">3</div>
               </div>
             </div>
           </div>
@@ -299,3 +305,91 @@ export function FinishedRoundCardLotteryAH() {
     </div>
   );
 }
+
+{
+  /* <div class="sc-3a5c8d1f-1 sc-32d5f017-0 iUtssi hBHgZr">
+  <svg
+    viewBox="0 0 32 32"
+    width="100%"
+    height="100%"
+    color="text"
+    xmlns="http://www.w3.org/2000/svg"
+    class="sc-4ba21b47-0 IIbzK"
+  >
+    <circle cx="16" cy="16" r="16" fill="#FFC43C"></circle>
+    <g opacity="0.1" style="mix-blend-mode: multiply;">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M24.3428 3.13245C28.9191 8.87189 28.5505 17.2575 23.2373 22.5707C17.528 28.28 8.27148 28.28 2.56223 22.5707C2.2825 22.291 2.01648 22.0028 1.76416 21.7068C4.02814 27.3487 9.54881 31.3327 16 31.3327C24.4683 31.3327 31.3332 24.4678 31.3332 15.9995C31.3332 10.6079 28.5504 5.86622 24.3428 3.13245Z"
+        fill="black"
+      ></path>
+    </g>
+    <g opacity="0.1" style="mix-blend-mode: multiply;">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M25.7714 4.18262C30.6309 10.2119 30.2608 19.061 24.661 24.6608C19.0616 30.2602 10.2134 30.6307 4.18408 25.7722C6.99655 29.1689 11.2456 31.3329 16.0001 31.3329C24.4685 31.3329 31.3334 24.468 31.3334 15.9997C31.3334 11.2446 29.1689 6.99508 25.7714 4.18262Z"
+        fill="black"
+      ></path>
+    </g>
+    <g style="mix-blend-mode: soft-light;">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M3.48969 24.8677C0.151051 18.7651 0.974979 11.0636 6.01931 6.01927C11.0639 0.974682 18.7659 0.15093 24.8687 3.49016C22.365 1.71201 19.3046 0.666603 16 0.666603C7.53165 0.666603 0.666733 7.53152 0.666733 15.9998C0.666733 19.3041 1.7119 22.3642 3.48969 24.8677Z"
+        fill="white"
+      ></path>
+    </g>
+    <g style="mix-blend-mode: soft-light;">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2.10087 9.51443C3.77283 5.93689 6.78541 3.11142 10.4922 1.68435C10.5461 1.73247 10.5988 1.78231 10.6504 1.83387C12.4839 3.6674 10.912 5.74432 8.66157 7.99477C6.41112 10.2452 4.33429 11.817 2.50076 9.98347C2.3535 9.83621 2.22025 9.67943 2.10087 9.51443Z"
+        fill="white"
+      ></path>
+    </g>
+  </svg>
+  <div class="sc-6a2c5391-0 iAUwDR">
+    <div
+      font-size="42px"
+      color="text"
+      class="sc-c56ebc7d-0 sc-6a2c5391-1 dYDvpe iuiryq"
+    >
+      3
+    </div>
+  </div>
+</div>;
+
+// -- -- -- -- -- -- -- --
+
+
+ {
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+
+
+// --------------------
+<div class="sc-6a2c5391-0 iAUwDR">
+  <div
+    font-size="42px"
+    color="text"
+    class="sc-c56ebc7d-0 sc-6a2c5391-1 dYDvpe iuiryq"
+  >
+    3
+  </div>
+</div>; */
+}
+// -- -- -- -- -- -- -- --
+// color: rgb(0, 0, 0);
+//     text-shadow: white -0.75px -0.75px 0px, white 0.75px -0.75px 0px, white -0.75px 0.75px 0px, white 0.75px 0.75px 0px;
+//     transform: rotate(27deg);
+//     // ...................
+
+//     color: var(--colors-text);
+//     font-weight: 600;
+//     line-height: 1.5;
+//     font-size: 42px;
