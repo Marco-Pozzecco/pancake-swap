79cb327cc522139d6a9df561408ef15facd35303
Milton Mariani <miltonmariani7@gmail.com>
2022-12-14 19:35:11 +0100

    Working on PrizePotCard component

--- src/components/card/prize-pot-card/PrizePotCard.js
+++ src/components/card/prize-pot-card/PrizePotCard.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./_prize-pot-card.scss";
 
 export function PrizePotCard() {
-  return <section> PrizePotCard (da creare)</section>;
+  return (
+    <section className="ppc-main-container-wrapper">
+      <section className="ppc-main-container">
+        <section className="ppc-sub-container">
+          {/* --- HEADER --- */}
+          <section className="ppc-top-header-container">
+            <button>Deposit</button>
+            <button>Claim</button>
+          </section>
+
+          {/* --- BODY --- */}
+          <section className="ppc-body-container">
+            <div className="ppcb-sub-container ppcb-sc-a">numbers</div>
+            <div className="ppcb-sub-container ppcb-sc-a">footer</div>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 }
