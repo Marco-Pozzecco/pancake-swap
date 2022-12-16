1d71a87f3405d1396c23f276400a25954ba6c479
Marco-Pozzecco <pozzecco.marco@gmail.com>
2022-12-16 16:59:46 +0100

    scrittura su file.json dei dati dello scraper (pools)

--- src/script/crawler.js
+++ src/script/crawler.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
 
 async function crawl() {
     const cardSelector = '.sc-a2a3a72d-1'
@@ -7,7 +8,7 @@ async function crawl() {
         const URL = "https://pancakeswap.finance/farms";
 
         const browser = await puppeteer.launch({
-            headless: false
+            headless: true
         })
 
         const page = await browser.newPage()
@@ -19,7 +20,7 @@ async function crawl() {
             await page.evaluate(() => {
                 window.scrollBy(0, window.innerHeight);
             });
-            await page.waitForTimeout(1200)
+            await page.waitForTimeout(500)
         }
         const data = await page.evaluate(() => {
             const trs = Array.from(document.querySelectorAll('tr')).map(y => [...Array.from(y.querySelectorAll('td')).map(x => x.textContent)]);
@@ -27,9 +28,27 @@ async function crawl() {
             return trs;
         })
 
+        // Process data
+        const json = JSON.stringify(data.map( row => {
+            return {
+                financialInstrument: row[0],
+                earned: "Earned",
+                amount: 0,
+                apr: row[2].match(/\d+\.\d+%|\d+\,\d+%|\d+%/g),
+                details: "Details"
+            }
+        }))
+        
+        // Writing to file
+        fs.writeFile("src/_data/scraper/scraperResult.json", json, (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("file written successfully in src/_data/scraper/")
+            }
+        })
 
-        console.log(data)
-
+        // Closing the puppeteer instance
         await browser.close()
 
     } catch (error) {
