0882a5e7c5399b2d767352536c4617e15246703b
Milton Mariani <miltonmariani7@gmail.com>
2022-12-17 20:11:22 +0100

    Fix

--- src/pages/earn/Farms.js
+++ src/pages/earn/Farms.js
@@ -3,12 +3,11 @@ import Subnav from "../../components/subnav/Subnav";
 import "./_earn.scss";
 
 export function Farms() {
-    return (
-        <div>
-            
-            <Subnav elements={["Farms", "Pools"]} />
-           
-            <h1 className="provaH1">Farms</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Subnav elements={["Farms", "Pools"]} />
+
+      <h1 className="provaH1">Farms</h1>
+    </div>
+  );
 }
