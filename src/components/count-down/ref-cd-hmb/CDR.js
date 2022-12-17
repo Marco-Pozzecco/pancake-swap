11591076562591d48bb701ebedf39746ee5b2706
Milton Mariani <miltonmariani7@gmail.com>
2022-12-17 14:45:11 +0100

    Add new countdown! with moment - need to install moment in every personal project

--- src/components/count-down/ref-cd-hmb/CDR.js
+++ src/components/count-down/ref-cd-hmb/CDR.js
@@ -11,18 +11,25 @@ export function CDR() {
 
   useEffect(() => {
     window.setInterval(() => {
-      const date = moment("31/12/2022 00:00:00", "DD/MM/YYYY HH:mm:ss");
+      const date = moment(
+        "31/12/2022 14:42:00",
+        "DD/MM/YYYY HH:mm:ss"
+      ); /*  Il 17 Dicembre dell anno 20222 siamo riusciti a far funzionare il countdown  */
       const diff = date.diff(moment(), "seconds");
-      const hours = Math.floor(diff / 60 / 60);
-      const minutes = Math.floor((diff - hours * 60 * 60) / 60);
-      const seconds = Math.floor(diff - hours * 60 * 60 - minutes * 60);
-      const hours2 = Math.floor(hours / 24);
+
       const days = Math.floor(diff / 24 / 60 / 60);
+
+      const hours = Math.floor((diff / (60 * 60)) % 24);
+
+      const minutes = Math.floor((diff / 60) % 60);
+
+      const seconds = Math.floor(diff % 60);
+      // -------------------------------------------------------------------------------------------
 
       setSecondi(seconds);
       setMinuti(minutes);
-      setOre(hours2);
       setGiorni(days);
+      setOre(hours);
     });
   });
 
